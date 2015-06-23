9c7c7b24773fe91aae576f667cc627eefabe8c75
Francis Bouvier <francis.bouvier@xerus-technologies.fr>
2015-06-23 11:45:22 -0700

    Builder: use os TempDir, fix path pb and use cache for tools.
    Signed-off-by: Francis Bouvier <francis.bouvier@xerus-technologies.fr>

--- src/builder/builder.go
+++ src/builder/builder.go
@@ -3,17 +3,19 @@ package builder
 import (
 	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"os"
+	"path"
 	"strings"
-	"time"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 
-	"github.com/francisbouvier/pipes/src/orch/swarm"
 	"github.com/francisbouvier/pipes/src/discovery"
+	"github.com/francisbouvier/pipes/src/orch/swarm"
+	"github.com/francisbouvier/pipes/src/utils"
 )
+
+const PIPES_CLIENT = "https://s3-us-west-1.amazonaws.com/pipesdocker/pipes_client"
 
 type categorization struct {
 	execType        string
@@ -22,20 +24,20 @@ type categorization struct {
 }
 
 var (
-	pythonCategory       = categorization{
-		execType: "python",
+	pythonCategory = categorization{
+		execType:        "python",
 		baseDockerImage: "python",
-		command: "python ",
+		command:         "python ",
 	}
-	rubyCategory         = categorization{
-		execType: "ruby", 
+	rubyCategory = categorization{
+		execType:        "ruby",
 		baseDockerImage: "ruby",
-		command: "ruby ",
+		command:         "ruby ",
 	}
 	simpleBinaryCategory = categorization{
-		execType: "binary",
+		execType:        "binary",
 		baseDockerImage: "microbox/scratch",
-		command: "",
+		command:         "",
 	}
 )
 
@@ -46,18 +48,18 @@ func check(e error) {
 	}
 }
 
-func BuildDockerImagesFromExec(args []string, c *cli.Context) (err error){ //execPath_category_map *map[string]categorization) {
+func BuildDockerImagesFromExec(args []string, c *cli.Context) (err error) { //execPath_category_map *map[string]categorization) {
 	var exec_paths []string
 
 	// Get input mode
-	for _,arg := range args {
+	for _, arg := range args {
 		arg_split_array := strings.SplitN(arg, ":", -1)
 		service_path := arg_split_array[0]
 		exec_paths = append(exec_paths, service_path)
 		service_name_array := strings.SplitN(service_path, "/", -1)
 		service_name := service_name_array[len(service_name_array)-1]
 		input_mode := "stdin"
-		if len(arg_split_array) > 1 { 
+		if len(arg_split_array) > 1 {
 			input_mode = arg_split_array[1]
 		}
 		err = WriteModeInStore(c, service_name, input_mode)
@@ -65,11 +67,11 @@ func BuildDockerImagesFromExec(args []string, c *cli.Context) (err error){ //exe
 			return err
 		}
 	}
-	
+
 	execs_map, err := AssociateExecWithType(c, exec_paths)
 	if err != nil {
 		log.Fatalln(err)
-	}	
+	}
 	fmt.Println()
 
 	// Iterating through the map
@@ -77,6 +79,8 @@ func BuildDockerImagesFromExec(args []string, c *cli.Context) (err error){ //exe
 		tmp_dir_path, new_exec_path, exec_file_name := SetTempDirectory(execOriginalPath)
 		imageName := CreateDockerfile(tmp_dir_path, new_exec_path, exec_file_name, category)
 		err = DockerBuild(c, tmp_dir_path, imageName)
+		// Delete TempDir
+		defer os.RemoveAll(tmp_dir_path)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -116,19 +120,19 @@ func AssociateExecWithType(c *cli.Context, exec_paths []string) (execPath_catego
 	return
 }
 
-func WriteCommandInStore(c *cli.Context, service_name string, command string) (error) {
+func WriteCommandInStore(c *cli.Context, service_name string, command string) error {
 	st, err := discovery.GetStore(c)
 	if err != nil {
 		return err
 	}
 	err = st.Write("command", command, fmt.Sprintf("services/%s", service_name))
 	if err != nil {
-			return err
-	}	
+		return err
+	}
 	return err
 }
 
-func WriteModeInStore(c *cli.Context, service_name string, input_mode string) (error) {
+func WriteModeInStore(c *cli.Context, service_name string, input_mode string) error {
 	st, err := discovery.GetStore(c)
 	if err != nil {
 		return err
@@ -136,34 +140,36 @@ func WriteModeInStore(c *cli.Context, service_name string, input_mode string) (e
 	err = st.Write("input_mode", input_mode, fmt.Sprintf("services/%s", service_name))
 	if err != nil {
 		return err
-	}	
+	}
 	return err
 }
 
 // Set a temp directory and cp the exec in it
 func SetTempDirectory(old_exec_path string) (tmp_dir_path, new_exec_path, exec_file_name string) {
 	// mkdir a tmp dir
-	/// generate random dir name from wd/tmp
-	wd, _ := os.Getwd()
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	random_nbr := r.Int63()
-	tmp_dir_path = fmt.Sprintf("%s/tmp/%d", wd, random_nbr)
-	// fmt.Printf("tmp_dir_path=%s\n", tmp_dir_path)
-	/// mkdir cmd
-	err := os.MkdirAll(tmp_dir_path, 0777)
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
-	}
+	tmp_dir_path, err := ioutil.TempDir("", "pipes_")
+	check(err)
 
 	// cp exec in that tmp dir
-	/// get exec name (and not path) to create new path for it
-	exec_file_name_array := strings.SplitN(old_exec_path, "/", -1)
-	exec_file_name = exec_file_name_array[len(exec_file_name_array)-1]
-	new_exec_path = tmp_dir_path + "/" + exec_file_name
-	// fmt.Printf("new exec path=%s\n", new_exec_path)
+	info, err := os.Stat(old_exec_path)
+	check(err)
+	exec_file_name = info.Name()
+	new_exec_path = path.Join(tmp_dir_path, exec_file_name)
 	/// cp cmd
-	os.Link(old_exec_path, new_exec_path)
+	/// TODO: check if there is no better way (buffio)
+	data, err := ioutil.ReadFile(old_exec_path)
+	check(err)
+	err = ioutil.WriteFile(new_exec_path, data, 0755)
+	check(err)
+
+	// Add pipes_client
+	p, err := utils.GetTool("pipes_client", PIPES_CLIENT)
+	check(err)
+	fmt.Println(p)
+	data, err = ioutil.ReadFile(p)
+	check(err)
+	err = ioutil.WriteFile(path.Join(tmp_dir_path, "pipes_client"), data, 0755)
+	check(err)
 
 	return
 }
@@ -178,14 +184,10 @@ func CreateDockerfile(tmp_dir_path string, new_exec_path string, exec_file_name 
 	entryPoint := fmt.Sprintf("bin/%s", exec_file_name)
 
 	// cp the templates/Dockerfile into the tmp dir
-	wd, _ := os.Getwd()
-	oldTemplateDockerfilePath := wd + "/src/builder/templates/Dockerfile"
 	newDockerfilePath := tmp_dir_path + "/Dockerfile"
 
 	// read new Dockerfile
-	data, err := ioutil.ReadFile(oldTemplateDockerfilePath)
-	check(err)
-	DockerfileString := string(data)
+	DockerfileString := T_GENERIC
 
 	// replace the placeholders to build the acutal Dockerfile
 	replaceBaseImage := strings.NewReplacer("<BASE_IMAGE>", category.baseDockerImage)
--- src/builder/templates.go
+++ src/builder/templates.go
@@ -1,9 +1,13 @@
+package builder
+
+const T_GENERIC = `
 FROM <BASE_IMAGE>
 
 ADD <EXEC_PATH_SRC> <EXEC_PATH_DEST>
 
 # Get the pipes_client
-ADD https://s3-us-west-1.amazonaws.com/pipesdocker/pipes_client bin/pipes_client
+ADD pipes_client bin/pipes_client
 RUN chmod 755 bin/pipes_client
 
 ENTRYPOINT ["pipes_client"]
+`
